943ce5f16d1848c7f034cdca5b8280793caa69ef
Christian Castillo <castillochristian3@gmail.com>
2022-09-10 13:28:58 -0400

    commit tree 82762637fbf5ad3fc3b45f501e91b1ea12941dab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        @word_array = word_array
+        @char_array = @word_array.map{|word| word.chars}
+        @anagram_list = @char_array.filter{|char| char.sort == @word.split("").sort}
+        @anagrams = @anagram_list.map{|anagram| anagram.join("")}
+    end    
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w[enlists google inlets banana])
+# binding.pry
